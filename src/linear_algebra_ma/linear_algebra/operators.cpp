5d8f8233774d21f9bb5af9659313e66bf3f3d3de
MatteoAlbi <matteo.albi99@gmail.com>
2023-08-01 14:00:49 +0200

    minor fix

--- src/linear_algebra_ma/linear_algebra/operators.cpp
+++ src/linear_algebra_ma/linear_algebra/operators.cpp
@@ -299,7 +299,7 @@ Matrix operator*(const Matrix& m1, const Matrix& m2){
 }
 #pragma endregion multiply
 
-#pragma region right_divide
+#pragma region divide
 void Matrix::operator/=(const double & k){
     for(uint i=0; i<this->_r; i++){
         for(uint j=0; j<this->_c; j++){
@@ -365,7 +365,7 @@ Matrix operator/(const Matrix& m1, const Matrix& m2){
     return m1*inv;
 }
 */
-#pragma endregion right_divide
+#pragma endregion divide
 
 #pragma region concatenate
 void Matrix::operator&=(const Matrix & m){
